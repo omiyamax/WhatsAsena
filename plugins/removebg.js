63752b235a057b88c66b3e843459c9cf57a4e501
Omindu Anjana <93399269+OminduAnjane@users.noreply.github.com>
2022-10-04 07:06:16 +0530

    Update removebg.js

--- plugins/removebg.js
+++ plugins/removebg.js
@@ -7,7 +7,7 @@ WhatsAsena - Yusuf Usta
 */
 
 const Asena = require('../events');
-const {MessageType, Mimetype} = require('@adiwajshing/baileys');
+const {MessageType, Mimetype} = require('@ominduanjane/whatsapp-web-api');
 const Config = require('../config');
 const fs = require('fs');
 const got = require('got');
