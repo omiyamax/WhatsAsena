3f6ac05e4b81e74c11f02fa895e4861c113d0508
Omindu Anjana <93399269+OminduAnjane@users.noreply.github.com>
2022-10-04 07:11:30 +0530

    Update web.js

--- plugins/web.js
+++ plugins/web.js
@@ -7,7 +7,7 @@ WhatsAsena - Yusuf Usta
 */
 
 const Asena = require('../events');
-const {MessageType} = require('@adiwajshing/baileys');
+const {MessageType} = require('@ominduanjane/whatsapp-web-api');
 const speedTest = require('@lh2020/speedtest-net');
 
 const Language = require('../language');
