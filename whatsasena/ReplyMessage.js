7297614150a56407363df6d2daa198d7e74b7535
Omindu Anjana <93399269+OminduAnjane@users.noreply.github.com>
2022-10-04 06:51:16 +0530

    Update ReplyMessage.js

--- whatsasena/ReplyMessage.js
+++ whatsasena/ReplyMessage.js
@@ -6,7 +6,7 @@ you may not use this file except in compliance with the License.
 WhatsAsena - Yusuf Usta
 */
 
-const {MessageType, Presence, MessageOptions} = require('@adiwajshing/baileys');
+const {MessageType, Presence, MessageOptions} = require('@ominduanjane/whatsapp-web-api');
 const Base = require('./Base');
 const Image = require('./Image');
 
