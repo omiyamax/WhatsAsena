801edd1814b3510cd0c7dbcd5df47a5018243965
Omindu Anjana <93399269+OminduAnjane@users.noreply.github.com>
2022-10-04 07:03:08 +0530

    Update neko.js

--- plugins/neko.js
+++ plugins/neko.js
@@ -8,7 +8,7 @@ WhatsAsena - Yusuf Usta
 */
 
 const Asena = require('../events');
-const {MessageType} = require('@adiwajshing/baileys');
+const {MessageType} = require('@ominduanjane/whatsapp-web-api');
 const got = require('got');
 
 const Language = require('../language');
